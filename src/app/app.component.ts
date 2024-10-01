b21e9771114b44a23c62b479a5ff8d13031c29ff
mohammadGhiasvand <m.ghiasvand.work@gmail.com>
2024-10-01 23:32:51 +0200

    keycloak authentication added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,9 @@ import {
   FaIconLibrary,
 } from '@fortawesome/angular-fontawesome';
 import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import { Oauth2AuthService } from './auth/oauth2-auth.service';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 @Component({
   selector: 'app-root',
@@ -33,15 +36,29 @@ import { fontAwesomeIcons } from './shared/font-awesome-icons';
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  title = 'Whatsappy';
+  title = 'whatsapp-clone-front';
 
   private faIconLibrary = inject(FaIconLibrary);
+  private oauth2Service = inject(Oauth2AuthService);
+  // toastService = inject(ToastService);
 
   ngOnInit(): void {
     this.initFontAwesome();
+    this.initAuthentication();
+    this.configDayJs();
+  }
+
+  private configDayJs() {
+    dayjs.extend(relativeTime);
+  }
+
+  private initAuthentication(): void {
+    this.oauth2Service.initAuthentication();
   }
 
   private initFontAwesome() {
     this.faIconLibrary.addIcons(...fontAwesomeIcons);
   }
+
+  protected readonly removeEventListener = removeEventListener;
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './auth/http-auth-interceptor';
+import { authExpiredInterceptor } from './auth/auth-expired-interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideHttpClient(
+      withInterceptors([authInterceptor, authExpiredInterceptor])
+    ),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+  ],
 };
